2bcc7a862c99c20ee17429a87a9abfdff049e269
yamashita8655 <wisdomword-justice@hotmail.co.jp>
2016-06-20 22:22:56 +0900

    更新

---
+++ ScoreCalculator/Assets/Scripts/ScoreInputScene/PlayerScoreListNode.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerScoreListNode : MonoBehaviour {
+    [SerializeField] InputField NameInputField;
+    [SerializeField] Text TotalText;
+    [SerializeField] Text RankText;
+    [SerializeField] ScrollRect ScoreScrollRext;
+
+    public void Setup() {
+    }
+
+    public void SetName(string name) {
+        NameInputField.text = name;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
